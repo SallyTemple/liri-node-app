856404e960222a7924008b98584df5e2bc69ff20
SallyTemple <pfende@gmail.com>
2018-04-08 22:37:52 -0700

    update

--- liri.js
+++ liri.js
@@ -1,1 +1,13 @@
 require("dotenv").config();
+var keys = require('./keys.js');
+var request = require('request');
+var twitter = require('twitter');
+var spotify = require('spotify');
+
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter); 
+var fs = require('fs');
+var filename = '/log.txt';
+var log = require('simple-node-logger').createSimpleFileLogger( filename );
+log.setLevel('all');
